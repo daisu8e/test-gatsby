39d7996bf06d59e14f0356bc425847670a0386c4
daisu8e <katsumata.coffee.150yen@gmail.com>
2020-04-20 14:11:01 -0700

    added stories of services/global-style

---
+++ src/views/services/global-style/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+
+import "./index.scss"
+
+storiesOf(`services/global-style`, module)
+.add(`default`, () => (
+  <>
+    <h1>xxxxx</h1>
+    <div>xxxxx</div>
+  </>
+))
